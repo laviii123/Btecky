e7e7c0f19bd20dd5dde7ae3670cc4d8a05b03d6f
Soumita Das <soumitad991@gmail.com>
2023-10-02 00:23:53 +0530

    Bulb On Bulb Off

---
+++ Bulb-On-Bulb-off/script.js
@@ -0,0 +1,8 @@
+on.addEventListener('click',function(e){
+    document.getElementById('bulb').src='https://i.postimg.cc/6QyTynzr/bulb-on.png'
+})
+
+
+off.addEventListener('click',function(e){
+    document.getElementById('bulb').src='https://i.postimg.cc/KjK1wL3c/bulb-off.png'
+})
