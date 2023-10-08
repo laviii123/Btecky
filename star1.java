0f258dbe78030e9e64cdd82bcb7f14c659a45792
Varad Chandrawar <115485413+Varad2k03@users.noreply.github.com>
2023-10-08 18:00:09 +0530

    Add files via upload

---
+++ star1.java
@@ -0,0 +1,12 @@
+public class star1 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+}
