6e08bed7d2799bc4850ad078fe5518e8437728e5
Raksha703 <85754170+Raksha703@users.noreply.github.com>
2023-10-22 22:56:32 +0530

    Create sieveOfEratosthenes.cpp

---
+++ sieveOfEratosthenes.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void sieveOfEratosthenes(int n) {
+    vector<bool> isPrime(n + 1, true);
+
+    for (int p = 2; p * p <= n; p++) {
+        if (isPrime[p]) {
+            for (int i = p * p; i <= n; i += p) {
+                isPrime[i] = false;
+            }
+        }
+    }
+
+    cout << "Prime numbers up to " << n << " are: ";
+    for (int p = 2; p <= n; p++) {
+        if (isPrime[p]) {
+            cout << p << " ";
+        }
+    }
+    cout << endl;
+}
+
+int main() {
+    int limit = 30;
+    sieveOfEratosthenes(limit);
+    return 0;
+}
